436e8e3441a6427b2819c902cc19be8c0b1ef24f
TANAKA Hidemune <auau@kne.biglobe.ne.jp>
2024-11-23 12:29:25 +0900

    Re New al Signed-off-by: TANAKA Hidemune <auau@kne.biglobe.ne.jp>

--- wmtetris/wmtetris.c
+++ wmtetris/wmtetris.c
@@ -52,6 +52,9 @@ int max_index = 0;
 int numbers[MAX_LINES];  // 数値を格納する配列
 int replay_mode = 0;
 int js_ax_num = 0;
+int rotate_idx = 0;
+int figure_type = 0;
+int next_figure_type = 0;
 
 static int buttons[6][4] = {
 	{ 0,  0, 64, 64},
@@ -62,24 +65,59 @@ static int buttons[6][4] = {
 	{43, 51, 60, 60}
 };
 
-static int initial_figures[7][4][2] = {
-	{ {0, 1}, {1, 1}, {2, 1}, {3, 1} },			// I
-	{ {0, 1}, {1, 1}, {2, 1}, {2, 2} },			// L
-	{ {0, 2}, {1, 2}, {2, 2}, {2, 1} },			// J
-	{ {0, 1}, {1, 0}, {1, 1}, {2, 1} },			// T
-	{ {0, 2}, {1, 2}, {1, 1}, {2, 1} },			//
-	{ {0, 1}, {1, 1}, {1, 2}, {2, 2} },			//
-	{ {1, 1}, {2, 1}, {1, 2}, {2, 2} }			// O
+static int initial_figures[7][4][4][2] = {
+	{
+		{{0, 1}, {1, 1}, {2, 1}, {3, 1}},
+		{{1, 0}, {1, 1}, {1, 2}, {1, 3}},
+		{{0, 1}, {1, 1}, {2, 1}, {3, 1}},
+		{{1, 0}, {1, 1}, {1, 2}, {1, 3}},
+	},			// I
+	{
+		{{0, 1}, {1, 1}, {2, 1}, {2, 2}},
+		{{0, 2}, {1, 0}, {1, 1}, {1, 2}},
+		{{0, 1}, {0, 2}, {1, 2}, {2, 2}},
+		{{1, 0}, {1, 1}, {1, 2}, {2, 0}},
+	},			// J
+	{
+		{{0, 2}, {1, 2}, {2, 2}, {2, 1}},
+		{{1, 0}, {1, 1}, {1, 2}, {2, 2}},
+		{{0, 1}, {0, 2}, {1, 1}, {2, 1}},
+		{{0, 0}, {1, 0}, {1, 1}, {1, 2}},
+	},			// L
+	{
+		{{0, 2}, {1, 1}, {1, 2}, {2, 2} },
+		{{1, 0}, {1, 1}, {1, 2}, {2, 1} },
+		{{0, 1}, {1, 1}, {1, 2}, {2, 1} },
+		{{0, 1}, {1, 0}, {1, 1}, {1, 2} },
+	},			// T
+	{
+		{{0, 2}, {1, 2}, {1, 1}, {2, 1} },
+		{{0, 0}, {0, 1}, {1, 1}, {1, 2} },
+		{{0, 2}, {1, 2}, {1, 1}, {2, 1} },
+		{{0, 0}, {0, 1}, {1, 1}, {1, 2} },
+	},			// S
+	{
+		{{0, 1}, {1, 1}, {1, 2}, {2, 2} },
+		{{1, 1}, {1, 2}, {2, 1}, {2, 0} },
+		{{0, 1}, {1, 1}, {1, 2}, {2, 2} },
+		{{1, 1}, {1, 2}, {2, 1}, {2, 0} },
+	},			// Z
+	{
+		{{1, 1}, {2, 1}, {1, 2}, {2, 2} },
+		{{1, 1}, {2, 1}, {1, 2}, {2, 2} },
+		{{1, 1}, {2, 1}, {1, 2}, {2, 2} },
+		{{1, 1}, {2, 1}, {1, 2}, {2, 2} },
+	}			// O
 };
 
 int which_button(int x, int y);
 int rotate_figure(int direction, int figure[4][2], int fig_x, int fig_y);
 void draw_figure(int figure[4][2], int type, int x, int y);
-void draw_next_figure(int figure[4][2], int type);
+void draw_next_figure(int figureW[4][2], int type);
 void general_draw_figure(int base_x, int base_y, int figure[4][2],
 						 int type, int x, int y);
 void full_refresh();
-int check_figure_position(int fig_x, int fig_y, int figure[4][2]);
+int check_figure_position(int fig_x, int fig_y, int figureW[4][2]);
 void append_to_file(const char *filepath, const char *content);
 
 // グローバル変数として display を定義
@@ -96,7 +134,7 @@ void setupJoystick() {
     }
 }
 
-void processJoystickInput(int *fig_x, int *fig_y, int figure[4][2], int figure_type, int *fast_mode) {
+void processJoystickInput(int *fig_x, int *fig_y, int figureW[4][2], int figure_type, int *fast_mode) {
     struct js_event js;
 	char str[20];     // 結果を格納する文字列バッファ
 	int pad_mask = 0;
@@ -137,20 +175,20 @@ void processJoystickInput(int *fig_x, int *fig_y, int figure[4][2], int figure_t
 
 
 	if (pad_mask & 1) {
-		rotate_figure(0, figure, *fig_x, *fig_y);
+		rotate_figure(0, figure_type, *fig_x, *fig_y);
 	}
 	if (pad_mask & 2) {
-		rotate_figure(1, figure, *fig_x, *fig_y);
+		rotate_figure(1, figure_type, *fig_x, *fig_y);
 	}
 	if (padX == -1) { // 左
 		pad_mask = pad_mask | 4;
-		if (check_figure_position(*fig_x - 1, *fig_y, figure)) {
+		if (check_figure_position(*fig_x - 1, *fig_y, figureW)) {
 			(*fig_x)--;
 		}
 	}
 	if (padX == 1) { // 右
 		pad_mask = pad_mask | 8;
-		if (check_figure_position(*fig_x + 1, *fig_y, figure)) {
+		if (check_figure_position(*fig_x + 1, *fig_y, figureW)) {
 			(*fig_x)++;
 		}
 	}
@@ -172,25 +210,25 @@ int get_number() {
 
 	return inp;
 }
-void processJoystickInputFromFile(int *fig_x, int *fig_y, int figure[4][2], int figure_type, int *fast_mode) {
+void processJoystickInputFromFile(int *fig_x, int *fig_y, int figureW[4][2], int figure_typeW, int *fast_mode) {
 
 	//Next Input
 	int inp = get_number();
 
 	// ボタンイベント処理
 	if (inp & 1) {
-				rotate_figure(0, figure, *fig_x, *fig_y);
+				rotate_figure(0, figureW, *fig_x, *fig_y);
 	}
 	if (inp & 2) {
-				rotate_figure(1, figure, *fig_x, *fig_y);
+				rotate_figure(1, figureW, *fig_x, *fig_y);
 	}
 	if (inp & 4) {
-			if (check_figure_position(*fig_x - 1, *fig_y, figure)) {
+			if (check_figure_position(*fig_x - 1, *fig_y, figureW)) {
 				(*fig_x)--;
 			}
 	}
 	if (inp & 8) {
-			if (check_figure_position(*fig_x + 1, *fig_y, figure)) {
+			if (check_figure_position(*fig_x + 1, *fig_y, figureW)) {
 				(*fig_x)++;
 			}
 	}
@@ -245,9 +283,8 @@ int score=0;
 
 int main(int argc, char *argv[]) {
 	int i, x, y, step, input, fast_mode, progress,
-		fig_x, fig_y, new_figure=1, figure_type, next_figure_type;
-	int figure[4][2] = { {0, 0}, {0, 0}, {0, 0}, {0, 0} },
-		next_figure[4][2] = { {0, 0}, {0, 0}, {0, 0}, {0, 0} };
+		fig_x, fig_y, new_figure=1;
+
 	unsigned long delay = INITIAL_DELAY, start_time;
 	XEvent event;
 	char str[20];     // 結果を格納する文字列バッファ
@@ -313,37 +350,31 @@ int main(int argc, char *argv[]) {
 	} else {
 		figure_type = get_number() & 7;
 	}
-	for (i = 0; i < 4; i++) {
-		figure[i][0] = initial_figures[figure_type][i][0];
-		figure[i][1] = initial_figures[figure_type][i][1];
-	}
+
 	if (replay_mode == 0) {
 		next_figure_type = random() % 7;
 		sprintf(str, "%d", next_figure_type); append_to_file(filepath, str);
 	} else {
 		next_figure_type = get_number() & 7;
 	}
-	for (i = 0; i < 4; i++) {
-		next_figure[i][0] = initial_figures[next_figure_type][i][0];
-		next_figure[i][1] = initial_figures[next_figure_type][i][1];
-	}
-	draw_next_figure(next_figure, next_figure_type);
+
+	draw_next_figure(initial_figures[next_figure_type][rotate_idx], next_figure_type);
 	fig_x = BOARD_WIDTH / 2 - 2;
 	fig_y = 0;
-	draw_figure(figure, figure_type, fig_x, fig_y);
+	draw_figure(initial_figures[figure_type][rotate_idx], figure_type, fig_x, fig_y);
 	RedrawWindow();
 
 	// Main Loop
 	while (1) {
-		if (check_figure_position(fig_x, fig_y + 1, figure)) {
+		if (check_figure_position(fig_x, fig_y + 1, initial_figures[figure_type][rotate_idx])) {
 			new_figure = 0;
-			draw_figure(figure, -1, fig_x, fig_y);
+			draw_figure(initial_figures[figure_type][rotate_idx], -1, fig_x, fig_y);
 			fig_y++;
 		} else {
 			new_figure = 1;
 			//fast_mode = 0;
 			for (i = 0; i < 4; i++) {
-				board [fig_x + figure[i][0]] [fig_y + figure[i][1]] = figure_type + 1;
+				board [fig_x + initial_figures[figure_type][rotate_idx][i][0]] [fig_y + initial_figures[figure_type][rotate_idx][i][1]] = figure_type + 1;
 			}
 
 			progress=0;
@@ -368,6 +399,8 @@ int main(int argc, char *argv[]) {
 				i /= 10;
 			}
 
+			rotate_idx = 0;
+
 			figure_type = next_figure_type;
 			if (replay_mode == 0) {
 				next_figure_type = random() % 7;
@@ -375,22 +408,17 @@ int main(int argc, char *argv[]) {
 			} else {
 				next_figure_type = get_number() & 7;
 			}
-			for (i = 0; i < 4; i++) {
-				figure[i][0] = next_figure[i][0];
-				figure[i][1] = next_figure[i][1];
-				next_figure[i][0] = initial_figures[next_figure_type][i][0];
-				next_figure[i][1] = initial_figures[next_figure_type][i][1];
-			}
-			draw_next_figure(next_figure, next_figure_type);
+
+			draw_next_figure( initial_figures[next_figure_type][rotate_idx] , next_figure_type);
 			fig_x = BOARD_WIDTH / 2 - 2;
 			fig_y = 0;
 		}
-		draw_figure(figure, figure_type, fig_x, fig_y);
+		draw_figure(initial_figures[figure_type][rotate_idx], figure_type, fig_x, fig_y);
 		RedrawWindow();
 
 		if (new_figure) {
 			// Game Over
-			if (!check_figure_position(fig_x, fig_y, figure)) {
+			if (!check_figure_position(fig_x, fig_y, initial_figures[figure_type][rotate_idx])) {
 				copyXPMArea(64, 64, 23, 15, 10, 24);
 				RedrawWindow();
 				while (1) {
@@ -428,9 +456,9 @@ int main(int argc, char *argv[]) {
 			if (js_fd == -1) {
 				// ジョイスティックなし
 				if (replay_mode) {
-					draw_figure(figure, -1, fig_x, fig_y);
-					processJoystickInputFromFile(&fig_x, &fig_y, figure, figure_type, &fast_mode);
-					draw_figure(figure, figure_type, fig_x, fig_y);
+					draw_figure(initial_figures[figure_type][rotate_idx], -1, fig_x, fig_y);
+					processJoystickInputFromFile(&fig_x, &fig_y, initial_figures[figure_type][rotate_idx], figure_type, &fast_mode);
+					draw_figure(initial_figures[figure_type][rotate_idx], figure_type, fig_x, fig_y);
 					RedrawWindow();
 				} else {
 					while (XPending(display)) {
@@ -448,36 +476,36 @@ int main(int argc, char *argv[]) {
 						}
 
 						if (input) {
-							draw_figure(figure, -1, fig_x, fig_y);
+							draw_figure(initial_figures[figure_type][rotate_idx], -1, fig_x, fig_y);
 							switch (input) {
 							case BUTTON_ROTATE_LEFT:
-								rotate_figure(0, figure, fig_x, fig_y);
+								rotate_figure(0, initial_figures[figure_type][rotate_idx], fig_x, fig_y);
 								break;
 							case BUTTON_ROTATE_RIGHT:
-								rotate_figure(1, figure, fig_x, fig_y);
+								rotate_figure(1, initial_figures[figure_type][rotate_idx], fig_x, fig_y);
 								break;
 							case BUTTON_MOVE_LEFT:
-								if (check_figure_position(fig_x - 1, fig_y, figure))
+								if (check_figure_position(fig_x - 1, fig_y, initial_figures[figure_type][rotate_idx]))
 									fig_x--;
 								break;
 							case BUTTON_MOVE_RIGHT:
-								if (check_figure_position(fig_x + 1, fig_y, figure))
+								if (check_figure_position(fig_x + 1, fig_y, initial_figures[figure_type][rotate_idx]))
 									fig_x++;
 								break;
 							case BUTTON_MOVE_DOWN:
 								fast_mode = 1;
 								break;
 							}
-							draw_figure(figure, figure_type, fig_x, fig_y);
+							draw_figure(initial_figures[figure_type][rotate_idx], figure_type, fig_x, fig_y);
 							RedrawWindow();
 						}
 					}
 				}
 			} else {
 				// ジョイスティックあり
-				draw_figure(figure, -1, fig_x, fig_y);
-				processJoystickInput(&fig_x, &fig_y, figure, figure_type, &fast_mode);
-				draw_figure(figure, figure_type, fig_x, fig_y);
+				draw_figure(initial_figures[figure_type][rotate_idx], -1, fig_x, fig_y);
+				processJoystickInput(&fig_x, &fig_y, initial_figures[figure_type][rotate_idx], figure_type, &fast_mode);
+				draw_figure(initial_figures[figure_type][rotate_idx], figure_type, fig_x, fig_y);
 				RedrawWindow();
 			}
 			usleep(TEMPORAL_RESOLUTION);
@@ -502,35 +530,19 @@ int which_button(int x, int y) {
 }
 
 int rotate_figure(int direction, int figure[4][2], int fig_x, int fig_y) {
-    int i, temp[4][2];
+	int wkR = rotate_idx;
+	int i;
 
-    // 回転中心を動的に計算
-    int center_x = 1; // 3×3 グリッドの中心
-    int center_y = 1; // 3×3 グリッドの中心
-
-    // 回転を計算
-    for (i = 0; i < 4; i++) {
-        // 中心からの相対座標を計算
-        int rel_x = figure[i][0] - center_x;
-        int rel_y = figure[i][1] - center_y;
-
-        // 回転後の座標を計算（時計回り or 反時計回り）
-        if (direction) { // 時計回り
-            temp[i][0] = center_x - rel_y;
-            temp[i][1] = center_y + rel_x;
-        } else { // 反時計回り
-            temp[i][0] = center_x + rel_y;
-            temp[i][1] = center_y - rel_x;
-        }
-    }
+	if (direction) { // 時計回り
+		wkR = (wkR + 1) % 4;
+	} else { // 反時計回り
+		wkR = (wkR + 3) % 4;
+	}
 
     // 回転後の位置が有効かチェック
-    if (check_figure_position(fig_x, fig_y, temp)) {
+    if (check_figure_position(fig_x, fig_y, initial_figures[figure_type][wkR])) {
         // 有効なら座標を更新
-        for (i = 0; i < 4; i++) {
-            figure[i][0] = temp[i][0];
-            figure[i][1] = temp[i][1];
-        }
+        rotate_idx = wkR;
         return 1;
     } else {
         // 無効なら回転をキャンセル
@@ -560,24 +572,24 @@ int rotate_figure(int direction, int figure[4][2], int fig_x, int fig_y) {
 */
 
 
-void draw_figure(int figure[4][2], int type, int x, int y) {
-	general_draw_figure(BOARD_POS_X, BOARD_POS_Y, figure, type, x, y);
+void draw_figure(int figureW[4][2], int type, int x, int y) {
+	general_draw_figure(BOARD_POS_X, BOARD_POS_Y, figureW, type, x, y);
 }
 
-void draw_next_figure(int figure[4][2], int type) {
+void draw_next_figure(int figureW[4][2], int type) {
 	copyXPMArea(64 + NEXT_POS_X, NEXT_POS_Y, BLOCK_SIZE * 4, BLOCK_SIZE * 4,
 				NEXT_POS_X, NEXT_POS_Y);
-	general_draw_figure(NEXT_POS_X, NEXT_POS_Y, figure, type, 0, 0);
+	general_draw_figure(NEXT_POS_X, NEXT_POS_Y, figureW, type, 0, 0);
 }
 
-void general_draw_figure(int base_x, int base_y, int figure[4][2],
+void general_draw_figure(int base_x, int base_y, int figureW[4][2],
 						 int type, int x, int y)
 {
 	int i, block_x, block_y;
 
 	for (i = 0; i < 4; i++) {
-		block_x = base_x + BLOCK_SIZE * (x + figure[i][0]);
-		block_y = base_y + BLOCK_SIZE * (y + figure[i][1]);
+		block_x = base_x + BLOCK_SIZE * (x + figureW[i][0]);
+		block_y = base_y + BLOCK_SIZE * (y + figureW[i][1]);
 		if (type == -1)
 			copyXPMArea(64 + block_x, block_y, BLOCK_SIZE, BLOCK_SIZE, block_x, block_y);
 		else
